b79e7c7425e14c1e601771493a54026e626ed298
Khaun02 <khaunx02@gmail.com>
2025-02-18 23:18:19 -0600

    leetcode: creation of leetcode #242
    - Creation of java file for leetcode #242 valid anagram
    - thought process: brute force and randomly arrange letters until you can get the word?
    - otherwise if no clue then created a checklist on Notion about general steps to solve

---
+++ easy/242_validAnagram_BruteForce.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static boolean isAnagram(String s, String t) {
+            
+        
+        return false;
+    }
+
+    public static void main(String[] args) {
+        // test case 1
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isAnagram(s, t));
+    }
+}
