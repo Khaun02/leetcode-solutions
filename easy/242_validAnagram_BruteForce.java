f780ed9cb13c83607c064e6fa65ba2c4857b65c0
Khaun02 <khaunx02@gmail.com>
2025-02-18 23:37:10 -0600

    leetcode: wrote thought process on how to solve
    - wrote a quick thought process on how i would solve this
    - will implement tommorow

--- easy/242_validAnagram_BruteForce.java
+++ easy/242_validAnagram_BruteForce.java
@@ -1,6 +1,8 @@
 class Solution {
     public static boolean isAnagram(String s, String t) {
-            
+        // first thought, since you have both s and t, if i were doing on paper
+        // i would just try to match up the letters from s to t in their
+        // respective index, will try to bruteforce thaq
         
         return false;
     }
