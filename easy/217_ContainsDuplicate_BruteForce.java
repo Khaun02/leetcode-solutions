479c41caa418279116fbbdead6cd255003733e52
Khaun02 <khaunx02@gmail.com>
2025-02-18 19:47:50 -0600

    leetcode: finished brute force for 217_containsDuplicate
    - biggest thing was to sort the array
    - by doing so you make it so that all the duplicates are next to each other which makes it really easy to compare
    - for loop just goes from nums[i] and nums[i+1] which i+1 is the number right in front of i
    - time complexity is O(n log n) since it sorts the array which uses timesort which is the slowest
    - explanation of O(n log n), the (log n) is for the array.sort then the n is by iterating through one by one for each item in the array
    - for further optimization can use hashmaps and check for duplicates first?

--- easy/217_ContainsDuplicate_BruteForce.java
+++ easy/217_ContainsDuplicate_BruteForce.java
@@ -1,9 +1,17 @@
+import java.util.Arrays;
+
 class Solution {
 
     public static boolean containsDuplicate(int[] nums) {
-        int numToCheck = nums[0];
+        // solution, sort the array, this means its all in order, so if like nums1[] would go to
+        // {1,1,2,3}, then the for method would check nums[i] which starts at 0 which would be 
+        // 1 == then for nums[i+1], that means to check the next one in front of it which would 
+        // be 1 == 1, which would make it true;
+        // the sort is the biggest thing here since it automatically makes it so that all the duplicates would be
+        // next to each other
+        Arrays.sort(nums);
         for (int i = 0; i < nums.length-1; i++) {
-            if (numToCheck == nums[i+1]) {
+            if (nums[i] == nums[i+1]) {
                 return true;
             }
         }
@@ -15,9 +23,13 @@ class Solution {
         int nums1[] = {1,2,3,1};
         int nums2[] = {1,2,3,4};
         int nums3[] = {1,1,1,3,3,4,3,2,4,2};
+        int nums4[] = {2,14,18,22,22};
+        int nums5[] = {2,3,100,22,22,100};
         
-        System.out.println(containsDuplicate(nums1));
-        System.out.println(containsDuplicate(nums2));
-        System.out.println(containsDuplicate(nums3));
+        System.out.println(containsDuplicate(nums1)); //true
+        System.out.println(containsDuplicate(nums2)); //false
+        System.out.println(containsDuplicate(nums3)); //true
+        System.out.println(containsDuplicate(nums4)); //true
+        System.out.println(containsDuplicate(nums5)); //true
     }
 }
