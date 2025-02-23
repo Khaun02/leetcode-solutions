15140923bc39644ac1db0553bb9d6a53014ad240
Khaun02 <khaunx02@gmail.com>
2025-02-22 23:13:46 -0600

    leetcode: completion of 01_twoSum_bruteForce
    - Implemented on 02/22/2025
    - issue 1: first i didnt know how to do it at all
    - solve 1: i finished the interview problem for skyward and learned to use a nested for loop to do calculations like this, so with that knowledge i implemented that for this leetcode
    issue 2: i was using the actual numbers that made the target instead of the indexes
    solve 2: swapped it so i use i and j instead of their value nums[i] and nums[j]
    issue 3: was stuck also with how to make an array into string
    solve 3: import java.util.Array then use the "Arrays.toString method
    issue 4: the comment explains
    solve 4: have it so you rearrange based on whether i or j is bigger

--- easy/01_twoSum_BruteForce.java
+++ easy/01_twoSum_BruteForce.java
@@ -1,20 +1,47 @@
+import java.util.*;
+
 class twoSum_BruteForce {
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         // thought process: go in order with for loop
         // i and then i+1, add then together and keep moving?
-        int[] num = new int[5];
 
-        return num;
+        // did this before with largestPalindrome
+        // brute force double for loop
+        int[] answer = new int[2];
+
+        // first stuck: nums=[2,5,5,11] target=10
+        // its giving [2,1] instead of [1,2]
+        for (int i = 0; i <= nums.length-1; i++) {
+            for (int j = 1; j <= nums.length-1; j++) {
+                if (nums[i] + nums[j] == target && i != j) {
+                    // this fixed the stuck!
+                    // fixed it so it goes it in smaller to bigger order?
+                    if (j > i) {
+                        answer[1] = j;
+                        answer[0] = i;
+                    }
+                    else {
+                        answer[0] = j;
+                        answer[1] = i;
+                    }
+                }
+            }
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {2,7,11,15};
-        int[] nums2 = {3,2,4};
-        int[] nums3 = {3,3};
+        int[] nums1 = {3,4,5,6};
+        int[] nums2 = {4,5,6};
+        int[] nums3 = {5,5};
 
-        int target1 = 9;
-        int target2= 6;
-        int target3 = 6;
+        int target1 = 7;
+        int target2 = 10;
+        int target3 = 10;
+        System.out.println(Arrays.toString(twoSum(nums1, target1)));
+        System.out.println(Arrays.toString(twoSum(nums2, target2)));
+        System.out.println(Arrays.toString(twoSum(nums3, target3)));
 
     }
 }
