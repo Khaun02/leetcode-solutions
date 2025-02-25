18554d6d50559cda4697e50526698d70347da6d4
Khaun02 <khaunx02@gmail.com>
2025-02-24 23:32:09 -0600

    leetcode: continued work on leetcode 42
    - did some short work on leetcode 42
    - stuck: not sure whether to use list or hashmap
    - sorting wise and how to make the groups grow dynamically?
    - maybe another helper method to sort...

--- medium/42_groupAnagrams_BruteForce.java
+++ medium/42_groupAnagrams_BruteForce.java
@@ -8,7 +8,10 @@
  * The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
  */
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 class groupAnagram_BruteForce {
     
@@ -16,17 +19,42 @@ class groupAnagram_BruteForce {
      * 
      * method that takes the array and returns if the index at position is a string
      * 
-     */
-    public static void isAnagram(String[] strs) {
-        String reversedString;
-        reversedString = strs[0];
-
+     * 02.21.25
         // thought process: double for loop again
         // check 1st against next by the reverse
         // if equal then that means its anagram
         // trouble 1: now that i cant just reverse it, how would i find if its an anagram...
-        // trouble 2: how to group them though...
+        // trouble 2: how to group them though...''
+     * 
+     * 02.24.25
+     * original thought process: i thought about it, to see if anagram just sort and see if its equal
+     * 
+     * q1: how do i know how many arraylist to create though...
+     * s1: chatgpt told me to check for unique identifier, still confused
+     * s1 cont: then gave me hint about keys and pairs, so first time implementing "hashmap"!
+     */
+    public static List<List<String>> isAnagram(String[] strs) {
+        
+        // declaration of variables
+        String sortedString;
+        sortedString = strs[0];
+        List<List<String>> masterAnagramList = new ArrayList<>();
+        List<String> anagramList = new ArrayList<>();
 
+        // first time implementing hashmap
+        HashMap<String, String> anagramPairs = new HashMap<String, String>();
+
+        // edge cases
+        if (strs.length == 0) {
+            return masterAnagramList;
+        }
+
+        // calculation 1: go through iteratively, sort and then add to the hashmap
+        for (int i = 0; i < strs.length-1; i++) {
+            // maybe helper method here?
+        }
+
+        return masterAnagramList;
     }
     
     public static void main(String[] args) {
